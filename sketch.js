8ce0b04f837b3a9bb82e1496b4aa1b0ea3fc5289
tanner noykoa <rust@tanners-MacBook.wifi.pcc.edu>
2020-02-06 11:40:44 -0800

    tidied up and finalized design

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var y;
 
 function setup() {
   // put setup code here
-  	createCanvas(500, 500);
+  	createCanvas(580, 500);
     background(0);
 
 }
@@ -15,11 +15,13 @@ function draw() {
   print(x,y);
 
   noStroke();
+  fill(0, 0, 255)
+  triangle(175, 380, 290, 60, 405, 380);
   fill(255, 0, 0)
   triangle(150, 375, 290, 100, 430, 375);
   fill(0, 255, 0);
   triangle(175, 360, 290, 125, 405, 360);
-  fill(0, 0, 255);
-  circle(290, 115, 10);
-  circle(309, 150, 10);
+  fill(0, 0, 0);
+  triangle(185, 355, 290, 135, 395, 355);
+
 }
